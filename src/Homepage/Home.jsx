7c705e89493b4ae03aae28ae571d73cba2d886f2
kencrimsonIT <tongkien930@gmail.com>
2025-09-15 23:01:25 +0700

    UPDATE: Add menu bar & information

--- src/Homepage/Home.jsx
+++ src/Homepage/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Menubar from "../Components/Menubar";
 import "./Home.css";
 
 const Home = () => {
     return (
-        <></>
+        <>
+            <Menubar />
+            <h1 className="full-name">Xin chào! Tôi là Tống Duy Kiên</h1>
+            <p className="department">Một newbie Full Stack Developer 🤓👨‍💻</p>
+        </>
     );
 };
 
