6b3d8e2b51d9c0796aeadc78099cef6f9653a6ab
kencrimsonIT <tongkien930@gmail.com>
2025-09-15 23:03:31 +0700

    UPDATE: Add menu bar button & submenu

--- src/Components/Menubar.jsx
+++ src/Components/Menubar.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import "../Layouts/Menubar.css";
+import {Menu} from "lucide-react";
 
 const Menubar = () => {
+    // Menu toggle handling
+    const [isOpened, setIsOpened] = useState(false);
+
+    const handlingMenu = () => {
+        setIsOpened(prev => !prev);
+    };
+
+    // Rendering
     return (
-        <></>
+        <>
+            <button className="menu-btn" onClick={handlingMenu}>
+                <Menu size={20}/>
+            </button>
+            {isOpened && (
+                <ul className={`submenu ${isOpened ? "show" : ""}`}>
+                    <li className="bio">About me</li>
+                    <li className="education">Education</li>
+                    <li className="skills">Skills</li>
+                    <li className="project">Project</li>
+                    <li className="contact">Contact</li>
+                </ul>
+            )}
+        </>
     );
 };
 
