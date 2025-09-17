452b311aa7e8f86859a132056f596ae42cdcf4f3
kencrimsonIT <tongkien930@gmail.com>
2025-09-17 23:26:34 +0700

    UPDATE: Add icon for contact informations

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "../Layouts/Contact.css";
+import {Mail, Phone, MapPin} from "lucide-react";
 
 const Contact = () => {
     return (
-        <></>
+        <>
+            <Mail size={22}/>
+            <Phone size={22}/>
+            <MapPin size={22}/>
+        </>
     );
 }
 
