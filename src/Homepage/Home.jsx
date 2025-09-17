fe33bdc6e6bb69ae91318c2fc6b383095950cbca
kencrimsonIT <tongkien930@gmail.com>
2025-09-17 23:23:51 +0700

    UPDATE: Add social media buttons into the homepage

--- src/Homepage/Home.jsx
+++ src/Homepage/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Menubar from "../Components/Menubar";
 import Avatar from "../Components/Avatar"
 import "./Home.css";
+import {Facebook, Github, Linkedin} from "lucide-react";
 
 const Home = () => {
     return (
@@ -11,6 +12,11 @@ const Home = () => {
                 <Avatar />
                 <h1 className="full-name">Xin chào! Tôi là Tống Duy Kiên</h1>
                 <p className="department">Một newbie Full Stack Developer 🤓👨‍💻</p>
+                <div className="icon-container">
+                    <button className="facebook-btn"><Facebook size={20}/></button>
+                    <button className="github-btn"><Github size={20}/></button>
+                    <button className="linkedin-btn"><Linkedin size={20}/></button>
+                </div>
             </div>
         </>
     );
