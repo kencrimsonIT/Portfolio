5412203b9d648ddc853ede2b9fd020aae4a89fa6
kencrimsonIT <tongkien930@gmail.com>
2025-09-17 11:32:32 +0700

    UPDATE: Add hero container for the introduction

--- src/Homepage/Home.jsx
+++ src/Homepage/Home.jsx
@@ -6,8 +6,10 @@ const Home = () => {
     return (
         <>
             <Menubar />
-            <h1 className="full-name">Xin chào! Tôi là Tống Duy Kiên</h1>
-            <p className="department">Một newbie Full Stack Developer 🤓👨‍💻</p>
+            <div className="hero-section">
+                <h1 className="full-name">Xin chào! Tôi là Tống Duy Kiên</h1>
+                <p className="department">Một newbie Full Stack Developer 🤓👨‍💻</p>
+            </div>
         </>
     );
 };
