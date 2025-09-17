6d100b24a4af680f3454e66d8a198fb5c25b074a
kencrimsonIT <tongkien930@gmail.com>
2025-09-17 17:10:02 +0700

    UPDATE: Add avatar component into the hero section

--- src/Homepage/Home.jsx
+++ src/Homepage/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Menubar from "../Components/Menubar";
+import Avatar from "../Components/Avatar"
 import "./Home.css";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <>
             <Menubar />
             <div className="hero-section">
+                <Avatar />
                 <h1 className="full-name">Xin chào! Tôi là Tống Duy Kiên</h1>
                 <p className="department">Một newbie Full Stack Developer 🤓👨‍💻</p>
             </div>
