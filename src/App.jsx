4088c2cfd97cef227158dcd6092bb2adbddd0ad9
kencrimsonIT <tongkien930@gmail.com>
2025-09-20 10:22:08 +0700

    UPDATE: Add education section into main frame

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from "./Homepage/Home";
+import Education from "./Components/Education"
 import Contact from "./Components/Contact";
 
 const App = () => {
   return (
       <>
         <Home />
+        <Education />
         <Contact />
       </>
   );
