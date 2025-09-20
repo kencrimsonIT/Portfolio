f8784be607a739805d345a90902a6935c52a1eb1
kencrimsonIT <tongkien930@gmail.com>
2025-09-20 10:20:50 +0700

    UPDATE: Add education informations

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -3,7 +3,15 @@ import "../Layouts/Education.css";
 
 const Education = () => {
     return (
-        <></>
+        <>
+            <h1 className="education-title">Học vấn</h1>
+            <ul className="education-info">
+                <li className="school"><strong>Trường:</strong> Đại học Nông Lâm TPHCM</li>
+                <li className="major"><strong>Ngành:</strong> Công nghệ thông tin</li>
+                <li className="course"><strong>Niên khóa:</strong> 2022 - 2027</li>
+                <li className="gpa"><strong>GPA:</strong> 2.68</li>
+            </ul>
+        </>
     );
 }
 
