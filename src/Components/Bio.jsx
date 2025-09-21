fc36f8fd011d67e7ccc0002ae6cd291e04bb359c
kencrimsonIT <tongkien930@gmail.com>
2025-09-21 22:53:57 +0700

    UPDATE: Add details for bio component

--- src/Components/Bio.jsx
+++ src/Components/Bio.jsx
@@ -3,7 +3,19 @@ import "../Layouts/Bio.css";
 
 const Bio = () => {
     return (
-        <></>
+        <>
+            <h1 className="bio-title">Về tôi</h1>
+            <div className="bio-info">
+                <p className="bio-intro">
+                    <h3 className="greetings"><strong>Xin chào! 👋 </strong></h3>
+                    Tôi là sinh viên năm cuối ngành CNTT tại Đại học Nông Lâm TP.HCM, đang tìm kiếm vị trí Intern
+                    Software Engineer. Hiện đã thành thạo ngôn ngữ Java, Kotlin, ReactJS, Spring Boot, và đã áp
+                    vào các dự án nhóm cũng như cá nhân về web app, mobile app, API back-end. Luôn mong muốn
+                    được đóng góp và học hỏi trong môi trường làm việc chuyên nghiệp, đồng thời nâng cao kỹ năng
+                    lập trình thực tế và cách thưc làm việc nhóm.
+                </p>
+            </div>
+        </>
     );
 }
 
